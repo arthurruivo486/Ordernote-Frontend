21b4a1262ae2f8700d5ef66e4a4a01b0883de59c
Artur Ruivo <arthurruivo486@gmail.com>
2025-11-25 23:29:37 -0300

    [feat] apk config

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_BASE = "https://h8gt5rj4-3000.brs.devtunnels.ms/api";
+const API_BASE = "https://bookish-halibut-wrv74p79xpjrfwjj-3000.app.github.dev/api";
 
 // Criar instância do axios
 const api = axios.create({
